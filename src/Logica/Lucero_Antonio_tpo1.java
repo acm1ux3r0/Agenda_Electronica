6046315cdbd2a26b2be25eb72d2dd668b20cf7e9
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-05-14 19:35:47 -0300

    Subida del Proyecto

---
+++ src/Logica/Lucero_Antonio_tpo1.java
@@ -0,0 +1,14 @@
+package Logica;
+
+import IGU.Principal;
+
+
+public class Lucero_Antonio_tpo1 {
+    
+    public static void main(String[] args) {
+        
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
+    }
+}
